bc20435ce7e05170826f6ed5425d4c977180939c
tsague <tsagueymele@yahoo.fr>
2017-01-17 16:23:02 +0100

    step 1 terminé

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,21 @@ use App\Twoot;
 Route::get('/', function () {
     // Récupérer tous les "twoots" et les ajouter à la vue
     return view('app')->with([
-        'twoots' => [] //Twoot::___()
+        'twoots' => Twoot::all()
     ]);
 });
 
 //Ici, décommenter et créer la route '/about'
-//Route::get('______', function(){
-//    return ____; //Renvoyer la vue 'about' !
-//});
+Route::get('/about', function(){
+    return view('about'); //Renvoyer la vue 'about' !
+});
 
 Route::post('twoots', function(){
-//    Twoot::______([
-//        'text' => request()->____
-//    ]);
-//
-//    return redirect()->to('_');
+    Twoot::create([
+       'text' => request()->text
+   ]);
+
+   return redirect()->to('');
 });
 
 Route::delete('twoots/{id}', function($id){
